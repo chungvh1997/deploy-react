3d3a6d27c2416d3b4ad59c0c435b76c25fe57385
chungvh <chungvh@runsystem.net>
2019-10-06 00:30:55 +0700

    Updates

---
+++ precache-manifest.6f64ed65b5fd367e61fc120399438e0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "822b1e16d7dc5f97e1647cb299cd2230",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "d62661bc77a1d2f25b12",
+    "url": "/deploy-react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "927edbb8708cc3045199",
+    "url": "/deploy-react/static/js/2.8c7a5489.chunk.js"
+  },
+  {
+    "revision": "d62661bc77a1d2f25b12",
+    "url": "/deploy-react/static/js/main.d19a2a4c.chunk.js"
+  },
+  {
+    "revision": "80858b539569e2c7a94e",
+    "url": "/deploy-react/static/js/runtime-main.51c5f801.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/deploy-react/static/media/logo.25bf045c.svg"
+  }
+]);
